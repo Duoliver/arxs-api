92d880682be8496395c236d9364ef5620d7db388
Douglas Oliveira <oliveira.douglasedu@outlook.com>
2023-12-09 00:42:04 -0300

    add name attrribute to entry entity and team access

--- ArxsAPI/Models/TeamChampionshipSeason.cs
+++ ArxsAPI/Models/TeamChampionshipSeason.cs
@@ -2,12 +2,17 @@ namespace ArxsAPI.Models
 {
     public class TeamChampionshipSeason : BaseModel
     {
+        public string Name { get; set; }
+
+
         public int TeamId { get; set; }
 
         public int ChampionshipSeasonId { get; set; }
 
         public int? CountryId { get; set; }
 
+
+        public Team Team { get; set; } = null!;
 
         public ChampionshipSeason ChampionshipSeason { get; set; } = null!;
 
@@ -16,6 +21,7 @@ namespace ArxsAPI.Models
         public ICollection<ChampionshipSeasonTrophy> Trophies { get; } = new List<ChampionshipSeasonTrophy>();
 
         public ICollection<TeamChampionshipSeasonDriver> DriversEntries { get; } = new List<TeamChampionshipSeasonDriver>();
+
 
         public TeamChampionshipSeason() { }
     }
