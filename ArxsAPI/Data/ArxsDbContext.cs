ce7f8106e627aabe54f637632c144c4af55749ea
Douglas Oliveira <oliveira.douglasedu@outlook.com>
2023-12-19 23:19:24 -0300

    create team table

--- ArxsAPI/Data/ArxsDbContext.cs
+++ ArxsAPI/Data/ArxsDbContext.cs
@@ -5,12 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ArxsAPI.Data
 {
-    public class ArxsDbContext : DbContext
+    public class ArxsDbContext(DbContextOptions<ArxsDbContext> options) : DbContext(options)
     {
-        public ArxsDbContext(DbContextOptions<ArxsDbContext> options)
-            : base(options)
-        {
-        }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -22,6 +20,19 @@ namespace ArxsAPI.Data
                 .ToTable("country")
                 .HasKey(c => c.Id);
             modelBuilder.ApplyConfiguration(new CountryMap());
+
+            modelBuilder.Entity<Team>()
+                .ToTable("team")
+                .HasKey(t => t.Id);
+            modelBuilder.Entity<Team>()
+                .HasOne(t => t.PreviousTeam)
+                .WithOne(pt => pt.NextTeam)
+                .HasForeignKey<Team>(t => t.PreviousTeamId);
+            modelBuilder.Entity<Team>()
+                .HasOne(t => t.Country)
+                .WithMany(c => c.Teams)
+                .HasForeignKey(t => t.CountryId);
+            modelBuilder.ApplyConfiguration(new TeamMap());
         }
     }
 }
