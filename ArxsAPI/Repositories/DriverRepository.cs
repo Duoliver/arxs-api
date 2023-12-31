8ed94ddcbe6886eb663a5d8eeabef3b9c31d4bc6
Douglas Oliveira <oliveira.douglasedu@outlook.com>
2023-12-31 16:07:56 -0300

    add addmany method to generic repo

--- ArxsAPI/Repositories/DriverRepository.cs
+++ ArxsAPI/Repositories/DriverRepository.cs
@@ -5,12 +5,5 @@ namespace ArxsAPI.Repositories
 {
     public class DriverRepository(ArxsDbContext context) : EntityRepository<Driver>(context)
     {
-        public async Task<ICollection<Driver>> AddMany(ICollection<Driver> drivers)
-        {
-            context.Drivers.AddRange(drivers);
-            await context.SaveChangesAsync();
-
-            return drivers;
-        }
     }
 }
